03f768849ae418a13336da694470471927c9333b
skygrass-il <61673499+skygrass-il@users.noreply.github.com>
2021-01-20 14:27:08 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,78 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+import 'bootstrap';
+import $ from 'jquery';
+import {ValidationObserver,ValidationProvider,extend,localize,configure} from 'vee-validate'; 
+import TW from 'vee-validate/dist/locale/zh_TW.json'
+import * as rules from 'vee-validate/dist/rules';
+
+
+import App from './App'
+import router from './router';
+import './bus';
+import currencyFilter from './filters/currency';
+import timeStamp from './filters/timestamp';
+
+
+Vue.use(VueAxios,axios);
+Vue.component('Loading',Loading);
+Vue.filter('currency',currencyFilter);
+Vue.filter('time',timeStamp);
+window.$ = $;
+Object.keys(rules).forEach((rule)=>{
+  extend(rule,rules[rule]);
+  })
+  localize('zh_TW',TW);
+  Vue.component('ValidationObserver',ValidationObserver);
+  Vue.component('ValidationProvider',ValidationProvider);
+  configure({
+  classes:{
+  valid:'is-valid',
+  invalid:'is-invalid'
+  }
+  });
+  
+
+Vue.config.productionTip = false;
+// axios.defaults.withCredentials = true;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  router,
+})
+
+router.beforeEach((to, from, next) => {
+  console.log('to',to,'from',from,'next',next);
+  if(to.meta.requiresAuth){
+    const api =`${process.env.APIPATH}/api/user/check`;
+    // console.log(process.env.APIPATH);
+    axios.post(api).then((response)=>{
+      console.log(response.data);
+      if(response.data.success){
+      next();
+      }else{
+        next({
+          path:'login',
+        });
+      }
+    });
+  }else{
+    next();
+  }
+})
+
+
+
+
+
+
+
+
---
+++ src/router/index.js
@@ -0,0 +1,112 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+// import Home from '@/components/HelloWorld';
+import Page from '@/components/pages/page';
+import child from '@/components/pages/child';
+import child2 from '@/components/pages/child2';
+import child3 from '@/components/pages/child3';
+import Menu from '@/components/pages/menu';
+import Login from '@/components/pages/Login';
+import Dashboard from '@/components/dashboard';
+import Products from '@/components/pages/Products';
+import Orders from '@/components/pages/Orders';
+import Coupons from '@/components/pages/Coupons';
+import CustomerOrder from '@/components/pages/CustomerOrders';
+import CustomerCheckout from '@/components/pages/CustomerCheckout';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+    linkActiveClass:'active',
+    routes:[
+        {
+            path:'*',
+            redirect:'login',
+        },
+        // {
+        //     name:'首頁',
+        //     path:'/',
+        //     component:Home,
+        //     meta: { requiresAuth: true },
+        // },
+        {
+            name:'Login',
+            path:'/',
+            component:Login,
+        },
+        {
+            name:'Dashboard',
+            path:'/admin',
+            component:Dashboard,
+            children:[
+                {
+                    name:'Products',
+                    path:'products',
+                    component:Products,
+                     meta: { requiresAuth: true },
+                }, 
+                {
+                    name:'Orders',
+                    path:'orders',
+                    component:Orders,
+                    meta: { requiresAuth: true },
+                }, 
+                {
+                    name:'Coupons',
+                    path:'coupons',
+                    component:Coupons,
+                    meta: { requiresAuth: true },
+                },
+            ]
+        },
+        {
+            name:'Dashboard',
+            path:'/admins',
+            component:Dashboard,
+            children:[
+                {
+                    name:'CustomerOrder',
+                    path:'customer_order',
+                    component:CustomerOrder,
+                },
+                {
+                  name:'CustomerCheckout',
+                  path:'customer_checkout/:orderId',
+                  component:CustomerCheckout,
+              },  
+            ]
+        },
+        {
+            // name:'分頁',
+            path:'/page',
+            // component:Page,
+            components:{
+                default:Page,
+                menu:Menu,
+            },
+            children:[
+                {
+                    name:'卡片1',
+                    path:'',
+                    component:child,
+                },
+                {
+                    name:'卡片2',
+                    path:'child2',
+                    component:child2,
+                },
+                {
+                    name:'卡片3',
+                    path:'child3',
+                    component:child3,
+                },
+            ]
+        },
+    ]
+})
+
+
+
+
+
